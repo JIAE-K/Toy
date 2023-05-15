6bcb292044a8a6ba3b90267c37d892ad43fa679a
JIAE-K <124761326+JIAE-K@users.noreply.github.com>
2023-05-15 16:52:16 +0900

    Update OutsideClickMenu.tsx

--- src/components/toolbar/Optional/OutsideClickMenu.tsx
+++ src/components/toolbar/Optional/OutsideClickMenu.tsx
@@ -5,25 +5,8 @@ import { setFontColor } from '../../../canvasController';
 import { useSelector } from 'react-redux';
 const OutsideClickMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const myRef = useRef<HTMLDivElement>(null);
-    const buttonRef = useRef<HTMLButtonElement>(null);
     const canvas = useSelector(state => (state as any).authoring).canvas
 
-    useEffect(()=> {
-        const handleClickOutside = (event: MouseEvent) => {
-            if(!myRef.current?.contains(event.target as Node) && !buttonRef.current?.contains(event.target as Node)) {
-                setIsOpen(false);
-            }
-        };
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    },[]);
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    }
     
     const onFontColorClick = () => {
         setFontColor(canvas);
@@ -31,9 +14,9 @@ const OutsideClickMenu = () => {
 
   return (
     <div>
-        <button className="mr-2 text-gray-500 border-gray-400 border rounded-md w-10 h-8 text-center cursor-pointer" ref={buttonRef} onClick={toggleMenu}><FormatColorText/></button>
+        <button onClick={() => setIsOpen(!isOpen)} className="mr-2 text-gray-500 border-gray-400 border rounded-md w-10 h-8 text-center cursor-pointer"><FormatColorText/></button>
         {isOpen && (
-            <div ref={myRef} className='absolute bg-white border border-gray-300 rounded shadow-lg mt-2'>
+            <div className='absolute bg-white border border-gray-300 rounded shadow-lg mt-2'>
                 <div className='p-2 flex'>
                     <div onClick={onFontColorClick} className="bg-black w-5 h-5 mr-2" />
                     <div onClick={onFontColorClick} className="bg-white border border-black w-5 h-5 mr-2" />
